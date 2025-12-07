ad570cfd639fd69f8cb8f7bbaa01c0447a539f9f
Shaik Abdul Munawar <shaikmunawar907@gmail.com>
2025-12-07 07:08:50 +0530

    Updated ts config and npm scripts

---
+++ client/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      Register
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ client/app/api/login/routes.ts
@@ -0,0 +1,1 @@
+// routes.ts/login/api
